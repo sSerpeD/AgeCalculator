b4f275dd0f44fb9006ab83c516e16e74ba052444
sSerpeD <80467475+sSerpeD@users.noreply.github.com>
2021-10-28 20:45:08 +0700

    Update AgeCalculator.py

--- AgeCalculator.py
+++ AgeCalculator.py
@@ -1,10 +1,30 @@
-num1 = int(input("Enter year of birth: "))
-result = int(2564) - num1
-result1 = int(2021) - num1
+def main():
 
-if num1 > int(2450):
-    print(result)
-else:
-    print(result1)
+    num1 = check()
+    calc(num1)
+
+def calc(num1):
+    result = int(2564) - (num1)
+    result1 = int(2021) - (num1)
+
+    if (num1) > int(2450):
+        print(f"Age: {result} year old")
+    else:
+        print(f"Age: {result1} year old")
+
+def check():
+    while True:
+        try:
+            num1 = int(input("Enter year of birth: "))
+            if (num1 > 2464 and num1 < 2564 or num1 > 1921 and num1 < 2021):
+                break
+            else:
+                print("Error: User have gone out of the age limit input...")
+        except ValueError:
+            print("Error: Input should be intergers...")
 
 
+    return num1
+
+
+main()
